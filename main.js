9ebfee5ca11218ea911bd05e70a90a936a2b24ad
HlmHc79 <hhernandezcaudron@gmail.com>
2022-05-23 09:43:29 -0600

    add meet the chef section

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-function myFunction() {
-    var x = document.getElementById("mobile-menu");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
+}
