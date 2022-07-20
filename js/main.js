6b2cad358fd1a7a504e943a653d7f6c09172e880
HlmHc79 <hhernandezcaudron@gmail.com>
2022-07-20 13:35:07 -0600

    making index responsive

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-function readMore() {
-  let dots = document.getElementById("dots");
-  let moreText = document.getElementById("more");
-  let btnText = document.getElementById("myBtn");
+// function toggleMobileMenu(menu) {
+//   menu.classList.toggle('open');
+// }
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
+
+// function readMore() {
+//   let dots = document.getElementById("dots");
+//   let moreText = document.getElementById("more");
+//   let btnText = document.getElementById("myBtn");
+
+//   if (dots.style.display === "none") {
+//     dots.style.display = "inline";
+//     btnText.innerHTML = "Read more";
+//     moreText.style.display = "none";
+//   } else {
+//     dots.style.display = "none";
+//     btnText.innerHTML = "Read less";
+//     moreText.style.display = "inline";
+//   }
+// }
+
+console.log(1+1);
+
